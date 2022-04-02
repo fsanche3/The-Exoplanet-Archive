bd454ad0fa506dc6f29edf2cce5cfdff0032824c
fsanche3 <60365292+fsanche3@users.noreply.github.com>
2022-04-01 21:22:52 -0400

    Add files via upload

---
+++ sqltest.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("Drivers:", sql.Drivers())
+	var err error
+	db, err := sql.Open("mysql", "fsanche3:isc496@tcp(pi.cs.oswego.edu:3306)/fsanche3_22S")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// this line confims connection
+	if err := db.Ping(); err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	fmt.Println("Succesfully")
+
+}
